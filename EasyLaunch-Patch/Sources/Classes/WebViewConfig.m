4e74c9153d2bb9cf0f93006c40414422fe436c5d
sanok52 <aleksandr.timofeev.98@bk.ru>
2026-05-19 17:14:57 +0800

    removeEasyLaunch

--- EasyLaunch-Patch/Sources/Classes/WebViewConfig.m
+++
@@ -1,9 +0,0 @@
-#import "WebViewConfig.h"
-
-BOOL const WebViewConfigShowCloseButton = YES;
-NSTimeInterval const WebViewConfigNavigationTimeout = 30.0;
-
-UIColor *WebViewConfigTintColor(void)
-{
-    return [UIColor colorWithRed:0.12 green:0.48 blue:0.96 alpha:1.0];
-}
